4e53548db6d5382e1e5e31287b1288e4eb4a19ce
Workbuddy <workbuddy@local>
2026-08-04 18:48:18 +0800

    Deploy Workbuddy 个人工作台 (PWA: manifest + icons + service worker)

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'workbuddy-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png',
+  './apple-touch-icon.png',
+  './favicon-32x32.png',
+  './favicon.ico'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
